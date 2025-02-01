85a1181136e5aea8a358fb2f24b43ef7cd668320
pranavpandey02 <pranavpandey479@gmail.com>
2025-02-01 17:18:08 +0530

    commting the value

---
+++ Fregments/src/App.jsx
@@ -0,0 +1,20 @@
+function App()
+{
+  return(
+    
+   <>
+    <center>
+    <h1>Healthy Food</h1>
+    </center>
+  <ul class="list-group">
+  <li class="list-group-item">Daal</li>
+  <li class="list-group-item">Green</li>
+  <li class="list-group-item">Roti</li>
+  <li class="list-group-item">Salad</li>
+  <li class="list-group-item">Rice</li>
+</ul>
+   </>
+   
+  )
+}
+export default App;
